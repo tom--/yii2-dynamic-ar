f41e7c16244bff6e57a9a34888c8217bbb099c92
tom-- <fsb@thefsb.org>
2015-01-25 08:47:48 -0500

    init

---
+++ DynamicActiveQuery.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace app\db;
+
+use Yii;
+use yii\base\Component;
+
+/**
+ * Dynamic columns can be used queries but DAQ requires a special syntax to identify
+ * that you are refering to a dynamic column. Owing to SQL's static typing you must
+ * also specify the type in the query. See createCommand() for specifics.
+ *
+ * Class DynamicActiveQuery
+ *
+ * @package app\db
+ */
+class DynamicActiveQuery extends \yii\db\ActiveQuery
+{
+    /**
+     * Maria-specific preparation for building a query that includes a dynamic column.
+     *
+     * @param \yii\db\QueryBuilder $builder
+     *
+     * @return \yii\db\Query
+     */
+    public function prepare($builder)
+    {
+        /** @var DynamicActiveRecord $modelClass */
+        $modelClass = $this->modelClass;
+        $dynCol = $modelClass::dynamicColumn();
+        if (!empty($dynCol)) {
+            if (empty($this->select)) {
+                $attrNames = array_keys($modelClass::getTableSchema()->columns);
+                $this->select = array_diff($attrNames, [$dynCol]);
+            }
+            $this->select[$dynCol] = 'COLUMN_JSON(' . $dynCol . ')';
+        }
+
+        return parent::prepare($builder);
+    }
+
+    /**
+     * Generate DB command from ActiveQuery with Maria-specific SQL for dynamic columns.
+     *
+     * This implementation is the best hack I could manage. A dynamic column name
+     * can appear anywhere that a normal column name could appear (select, join, where, ...).
+     * It needs to be converted to the Maria SQL for accessing a dynamic column.
+     * Because SQL is statically-typed and there is no schema to refer to for the dyn-cols,
+     * the accessor SQL must specify the the dyn-col's type, e.g.
+     *
+     *     WHERE COLUMN_GET(details, 'color' as char) = 'black'
+     *
+     * In which details is the blob column containing all the dynamic columns, 'color' is the
+     * name of a dynamic column that may or may not appear in any given table record, and
+     * char means the value should be cast to char before it is compared with 'black'
+     *
+     * So I faced two problems:
+     *    1. How to identify a dynamic column name in an ActiveQuery?
+     *    2. How to choose the type to which it should be cast?
+     *
+     * The operating and design concept of DynamicAR is "a column that doesn't appear in the
+     * schema is assumed to be a dynamic column". So to infer from an AQ instance the column names
+     * that need to be converted to dyn-col accessor SQL I need to go through the AQ to identify
+     * all the column names and remove those in the schema. But I don't know how to
+     * identify column names in an AQ instance. Even if I did, there's problem 2.
+     *
+     * The only way I can imagine to infer dynamic column type from an AQ instance is to look
+     * at the context. If the dyn-col is compared with a bound parameter, that's a possible
+     * approach. If it is being used in a function, e.g. CONCAT(), or being compared with a
+     * schema column, that suggests something. But if it is on its own in a SELECT then I am
+     * stuck. Also stuck if it is compared with another dyn-col. This seems fundamentally
+     * intractible to me.
+     *
+     * So I decided that the user needs to help ActiveQuery by distinguishing names that
+     * are dyn-col names and by explicitly specifying the type. The format I chose is:
+     *
+     *     <column|type>
+     *
+     * Omitting type implies the default type: char. Child dyn-col names are separated from
+     * parents with . (period), e.g. <address.country|char>. Spaces are not tolerated.
+     * So a user can do:
+     *
+     *     $blackShirts = Product::find()
+     *         ->where(['category' => Product::SHIRT, '<color>' => 'black'])
+     *         ->all();
+     *
+     *     $cheapShirts = Product::find()
+     *         ->where(['category' => Product::SHIRT])
+     *         ->andWhere('<price.retail.unit|decimal(6,2)> < 20.00')
+     *         ->all();
+     *
+     * The implementation follows db\Connection's quoting of [[string]] and {{string}}. Once
+     * the full SQL string is ready, preg_repalce it. The regex pattern here is a bit complex
+     * and the replacement callback isn't pretty either. Is there a better way to add to
+     * $params in the callback than this? And for the parameter placeholder counter $i?
+     *
+     * @param null|\yii\db\Connection $db
+     *
+     * @return \yii\db\Command
+     */
+    public function createCommand($db = null)
+    {
+        if ($db === null) {
+            $db = Yii::$app->getDb();
+        }
+        list ($sql, $params) = $db->getQueryBuilder()->build($this);
+
+        /** @var DynamicActiveRecord $modelClass */
+        $modelClass = $this->modelClass;
+        $dynCol = $modelClass::dynamicColumn();
+
+        $callback = function ($matches) use (&$params, $dynCol, &$i) {
+            $type = !empty($matches[2]) ? $matches[2] : 'char';
+            $sql = $dynCol;
+            $parts = explode('.', $matches[1]);
+            $nParts = count($parts);
+            foreach ($parts as $col) {
+                $i += 1;
+                $placeholder = DynamicActiveRecord::PARAM_PREFIX_QUERY . $i;
+                $params[$placeholder] = $col;
+                $sql = "COLUMN_GET($sql, $placeholder AS " . ($i == $nParts ? $type : 'char') . ')';
+            }
+            return $sql;
+        };
+
+        $start = '[a-z_\x7f-\xff]';
+        $cont = '[a-z0-9_\x7f-\xff]';
+        $l = '(?:\(\d[\d,]*\))?';
+        $type
+            = "binary$l|char$l|date|datetime$l|decimal$l|double$l|int(eger)?"
+            . "|signed(?: inte(eger)?)?|time$l|unsigned(?: inte(eger)?)?";
+        $pattern = "{ < ($start $cont* (?: \\. $cont+)*) (?: \\| ($type))? > }ux";
+
+        $i = 0;
+        $sql = preg_replace_callback($pattern, $callback, $sql);
+
+        return $db->createCommand($sql, $params);
+    }
+}
---
+++ DynamicActiveRecord.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace app\db;
+
+use Yii;
+
+/**
+ * Class DynamicActiveRecord
+ *
+ * Dynamic Active Record adds to Yii 2.0 Active Record unstructured attributes
+ * stored in Maria 10.0+ Dynamic Columns or PostgreSQL 9.4+ jsonb columns. It
+ * provides the features of a NoSQL DB within a column in an SQL RDBMS table.
+ *
+ * The design assumes that there is exactly one column in a DAR class's table
+ * to store the dynamic columns (dyn-cols), identified by the dynamicColumn()
+ * method. The main concept in the design is that you can read or write or
+ * write any attribute name and if it is not a schema column attribute or a
+ * defined by a virtual attribute getter/setter then it is assumed to be the
+ * name of a dyn-col.
+ *
+ * So, for example, if a DAR class Product represents the following Maria 10.0
+ * and detail is the column containing the dynamic columns...
+ *
+ * ```sql
+ * CREATE TABLE product (
+ *    id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ *    name varchar(100) NOT NULL,
+ *    sku char(10) NOT NULL,
+ *    details blob NOT NULL)
+ * ```
+ *
+ * ```php
+ * $shirt = new Product();
+ * $shirt->name = 'Men\s' Tee Shirt';
+ * $shirt->sku = 'STMA123456';
+ * $shirt->color = 'black';
+ * $shirt->size = 'L';
+ * $shirt->fabric = 'cotton';
+ * ```
+ *
+ * The name and sku are stored in normal columns identified in the schema. DAR
+ * recognizes that the other attributes, color, size and fabric, are not in the
+ * schema and saves them as dyn-cols instead.
+ *
+ * Note: DAR does not (at present) prevent writing to the blob column containing
+ * dynamic
+ *
+ * A dyn-col can contain structued data. At the top level they are accessed as
+ * though they were object properties, just like AR attributes. But the value
+ * may be a PHP array.
+ *
+ * ```php
+ * $shirt->price = [
+ *     'retail' => 12.99,
+ *     'wholesale' => [12 => 10.50, 60 => 9.50]
+ * ];
+ * ```
+ *
+ * After loading a DAR record, all its dyn-cols are available for reading.
+ *
+ * @package app\db
+ */
+abstract class DynamicActiveRecord extends \yii\db\ActiveRecord
+{
+    const PARAM_PREFIX_ATTRS = ':dca';
+    const PARAM_PREFIX_QUERY = ':dcq';
+
+    private $_dynamicAttributes = [];
+
+    public static function find()
+    {
+        return Yii::createObject(DynamicActiveQuery::className(), [get_called_class()]);
+    }
+
+    /**
+     * Maria-specific dynamic column blob decoding.
+     *
+     * @param string $encoded
+     *
+     * @return array mixed
+     */
+    public static function dynColDecodeMaria($encoded)
+    {
+        return json_decode($encoded, true);
+    }
+
+    /**
+     * Decode DB-specific encoded dynamic attribute blob.
+     * @param string $encoded Serialization of aynamc attributes in DB-specific form
+     * @return array Dynamic attributes in name => value pairs (possibly nested)
+     *
+     * @throws \yii\base\Exception
+     */
+    public static function dynColDecode($encoded)
+    {
+        throw new \yii\base\Exception('Missing DB-specific method: ' . static::className() . '..' . __METHOD__);
+    }
+
+    /**
+     * Maria-specific SQL for writing dynamic column fields
+     *
+     * @param array $attrs
+     * @param array $params
+     * @param string $prefix
+     *
+     * @return string
+     */
+    public static function dynColSqlMaria($attrs, &$params, $prefix)
+    {
+        $sql = [];
+        $i = 0;
+        foreach ($attrs as $key => $value) {
+            $i += 1;
+            $ph = $prefix . $i;
+            $sql[] = $ph . 'k';
+            $params[$ph . 'k'] = $key;
+            if (is_scalar($value)) {
+                $sql[] = $ph . 'v';
+                $params[$ph . 'v'] = $value;
+            } else {
+                $ph .= '_';
+                $sql[] = self::dynColSql((array) $value, $params, $ph);
+            }
+        }
+        return 'COLUMN_CREATE(' . implode(',', $sql) . ')';
+    }
+
+    /**
+     * Create the SQL and parameter bindings for setting attributes as dynamic fields in a DB record.
+     *
+     * @param array $attrs Name and value pairs of dynamic fields to be saved in DB
+     * @param array $params Expression parameters for binding, passed by reference
+     * @param string $prefix Base parameter placeholder prefix.
+     * @return string SQL for a DB Expression
+     * @throws \yii\base\Exception
+     */
+    public static function dynColSql($attrs, &$params, $prefix)
+    {
+        return static::dynColSqlMaria($attrs, $params, self::PARAM_PREFIX_ATTRS);
+    }
+
+    public static function dynColExpression($attrs)
+    {
+        if (!$attrs) {
+            return null;
+        }
+
+        $params = [];
+        $sql = static::dynColSql($attrs, $params, self::PARAM_PREFIX_ATTRS);
+        return new \yii\db\Expression($sql, $params);
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->_dynamicAttributes) && array_key_exists($name, $this->_dynamicAttributes)) {
+            return $this->_dynamicAttributes[$name];
+        }
+
+        return parent::__get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        try {
+            parent::__set($name, $value);
+        } catch (\yii\base\UnknownPropertyException $e) {
+            $this->_dynamicAttributes[$name] = $value;
+        }
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->_dynamicAttributes[$name]) || parent::__isset($name);
+    }
+
+    public function __unset($name)
+    {
+        if (array_key_exists($name, $this->_dynamicAttributes)) {
+            unset($this->_dynamicAttributes[$name]);
+        } else {
+            parent::__unset($name);
+        }
+    }
+
+    public static function dynamicColumn()
+    {
+        return null;
+    }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            $this->setAttribute(static::dynamicColumn(), self::dynColExpression($this->_dynamicAttributes));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param DynamicActiveRecord $record
+     * @param array $row
+     */
+    public static function populateRecord($record, $row)
+    {
+        $dynCol = static::dynamicColumn();
+        if (isset($row[$dynCol])) {
+            $record->_dynamicAttributes = static::dynColDecode($row[$dynCol]);
+        }
+        parent::populateRecord($record, $row);
+    }
+}
