674b8240210b84b8bf83e11a3bff507e2bed3e3b
tom-- <fsb@thefsb.org>
2015-05-20 15:41:30 -0400

    add tests for remaining DAR methods

--- tests/unit/DynamicActiveRecordTest.php
+++ tests/unit/DynamicActiveRecordTest.php
@@ -33,7 +33,10 @@ class DynamicActiveRecordTest extends ActiveRecordTest
         $this->db = BaseRecord::$db = $this->getConnection(self::$resetFixture);
     }
 
-    public function testSetAttribute()
+    /**
+     * Also test __get()
+     */
+    public function testSetGetAttribute()
     {
         self::$resetFixture = false;
         $array = [
@@ -55,31 +58,99 @@ class DynamicActiveRecordTest extends ActiveRecordTest
         ];
 
         $p = new Product();
-        $p->array = $array;
+        $p->setAttribute('array', $array);
 
         $array['d'] = 4;
         $p->setAttribute('array.d', 4);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals(4, $p->getAttribute('array.d'));
 
         $array['e']['a']['b']['c'] = 5;
         $p->setAttribute('array.e.a.b.c', 5);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals(5, $p->getAttribute('array.e.a.b.c'));
 
         $array['c']['b']['c'] = 6;
         $p->setAttribute('array.c.b.c', 6);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals(6, $p->getAttribute('array.c.b.c'));
 
         $array['c']['c']['b']['d'] = ['x' => 1, 'y' => ['z' => 2]];
         $p->setAttribute('array.c.c.b.d', ['x' => 1, 'y' => ['z' => 2]]);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals(['x' => 1, 'y' => ['z' => 2]], $p->getAttribute('array.c.c.b.d'));
 
         $array['c']['c']['c'] = 7;
         $p->setAttribute('array.c.c.c', 7);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals(7, $p->getAttribute('array.c.c.c'));
 
         $array['c']['c'] = [9, 8, 7, 6];
         $p->setAttribute('array.c.c', [9, 8, 7, 6]);
         $this->assertEquals($array, $p->array);
+        $this->assertEquals($array, $p->getAttribute('array'));
+        $this->assertEquals([9, 8, 7, 6], $p->getAttribute('array.c.c'));
+    }
+
+    public function testIssetUnsetAttribute()
+    {
+        $p = new Product();
+
+        $this->assertFalse($p->issetAttribute('doesntexist'));
+        $p->unsetAttribute('doesntexist');
+        $this->assertFalse($p->issetAttribute('doesntexist'));
+
+        // unset column attribute
+        $this->assertFalse($p->issetAttribute('name'));
+
+        $p->name = 'spinitron';
+        $this->assertTrue($p->issetAttribute('name'));
+        $p->unsetAttribute('name');
+        $this->assertFalse($p->issetAttribute('name'));
+
+        $p->setAttribute('array.c.c.b.d', ['x' => 1, 'y' => ['z' => 2]]);
+        $this->assertTrue($p->issetAttribute('array.c.c.b.d.y.z'));
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d.doesntexist'));
+
+        $p->unsetAttribute('array.c.c.b.d.doesntexist');
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d.doesntexist'));
+
+        $p->unsetAttribute('array.c.c.b');
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d.doesntexist'));
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d.y.z'));
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d.y'));
+        $this->assertFalse($p->issetAttribute('array.c.c.b.d'));
+        $this->assertFalse($p->issetAttribute('array.c.c.b'));
+        $this->assertTrue($p->issetAttribute('array.c.c'));
+    }
+
+    public function testGetNullAttribute()
+    {
+        $p = new Product();
+        $this->assertNull($p->doesntexist);
+        $this->assertNull($p->name);
+        $p->setAttribute('array.c.c.b.d', ['x' => 1, 'y' => ['z' => 2]]);
+        $this->assertNotNull($p->getAttribute('array.c.c.b.d.y.z'));
+        $this->assertNull($p->getAttribute('array.c.c.b.d.y.z.doesntexist'));
+        $this->assertNull($p->getAttribute('array.c.doesntexist'));
+        $p->unsetAttribute('array.c.c.b');
+        $this->assertNull($p->getAttribute('array.c.c.b.d.y.z'));
+        $this->assertNull($p->getAttribute('array.c.c.b'));
+    }
+
+    /**
+     * @expectedException \yii\base\InvalidCallException
+     * @expectedExceptionMessage Invalid attribute name "4chan"
+     */
+    public function testInvalidAttributeName()
+    {
+        $p = new Product();
+        $p->__set('4chan', 'sucks');
     }
 
     public function testReadBinary()
@@ -232,7 +303,7 @@ class DynamicActiveRecordTest extends ActiveRecordTest
             ['octet', DynamicActiveRecord::DATA_URI_PREFIX . base64_encode('This is my string')],
         ];
 
-        $data = array_merge($data, include(__DIR__ . '/unicodeStrings.php'));
+        //$data = array_merge($data, include(__DIR__ . '/unicodeStrings.php'));
 
         $data[] = ['binarystring', self::BINARY_STRING];
 
@@ -314,6 +385,7 @@ class DynamicActiveRecordTest extends ActiveRecordTest
     {
         self::$resetFixture = false;
         $product = new Product();
+        /** @var string $product->$name */
         $product->$name = $value;
 
         $product->save(false);
